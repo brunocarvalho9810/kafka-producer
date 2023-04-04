0f05a097c99a58e78742c3dc559a2729a5980824
Bruno Carvalho <brunocarvalho9810@gmail.com>
2023-04-04 20:42:44 -0300

    Adicionando diagrama

--- src/main/java/com/kafka/producer/controller/TestController.java
+++ src/main/java/com/kafka/producer/controller/TestController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.util.stream.IntStream;
 
 /**
